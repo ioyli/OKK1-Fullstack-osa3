7a0eb1381e0b2744ddd7ca640c9ab6e0c7880799
ioyli <veera.yli-kleemola@esedulainen.fi>
2022-10-12 08:20:39 +0300

    3.7.-3.8.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :posted'))
+
+morgan.token('posted', function(req, res) {
+    return JSON.stringify(req.body)
+})
 
 let persons = [
     {
